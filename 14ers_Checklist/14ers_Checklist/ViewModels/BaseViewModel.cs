6b07bbf08c8c795c447f9801fbe62e90b90ed960
MattBubernak <soccerfreak24@gmail.com>
2014-07-19 22:54:12 -0600

    basic functioanlity for checklistviewmodel and baseviewmodel

---
+++ 14ers_Checklist/14ers_Checklist/ViewModels/BaseViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel;
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using _14ers_Checklist.Models; 
+
+namespace _14ers_Checklist.ViewModels
+{
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+
+        protected DataBaseContext db;
+
+        //needed for observer pattern/notify property changed 
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void NotifyPropertyChanged(String propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (null != handler)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
---
+++ 14ers_Checklist/14ers_Checklist/ViewModels/ChecklistViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14ers_Checklist.ViewModels
+{
+    class ChecklistViewModel : BaseViewModel
+    {
+        private ChecklistViewModel _checkListViewModel;
+        public ObservableCollection<MountainViewModel> mountains; 
+        
+        private ChecklistViewModel()
+        {
+            this._checkListViewModel = new ChecklistViewModel();
+            populate_mountains(); 
+        }
+
+        public ChecklistViewModel getInstance()
+        {
+            return _checkListViewModel; 
+        }
+
+        public void populate_mountains()
+        {
+            //populate the mountains list from the DB 
+        }
+    }
+}
